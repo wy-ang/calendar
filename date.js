1412fa514e5b85d20b6608dad1ecb1ffedcc4043
wangyang <wyang1117@msn.com>
2016-07-11 20:46:34 +0800

    添加样式及判断是否是过去时间

--- date.js
+++ date.js
@@ -7,10 +7,9 @@ $(function(){
 
 		//构建时间段
 		var weekTxt = ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
+		var hourTxt = ['08:00 - 09:00', '09:00 - 10:00', '10:00 - 11:00', '11:00 - 12:00', '13:00 - 14:00', '14:00 - 15:00', '15:00 - 16:00', '16:00 - 17:00', '17:00 - 18:00', '18:00 - 19:00', '19:00 - 20:00', '20:00 - 21:00', '21:00 - 22:00', '22:00 - 23:00'];
 		var hourDraw = function(){
-			var hourTxt = ['06:00 - 07:00', '07:00 - 08:00', '08:00 - 09:00', '09:00 - 10:00', '10:00 - 11:00', '11:00 - 12:00', '12:00 - 13:00', '13:00 - 14:00', '14:00 - 15:00', '15:00 - 16:00', '16:00 - 17:00', '17:00 - 18:00', '18:00 - 19:00', '19:00 - 20:00', '20:00 - 21:00', '21:00 - 22:00', '22:00 - 23:00', '23:00 - 24:00'],
-				hourPatText = ['上<br/>午', '下<br/>午', '晚<br/>上'],
-				a = ['1','2','3','4','5','6','7'],
+				var hourPatText = ['上<br/>午', '下<br/>午', '晚<br/>上'],
 				h,
 				s,
 				hourEm = '';
@@ -20,7 +19,7 @@ $(function(){
 					}else{
 						hourEm += '<ul><li>';
 					}
-					hourEm += '<span>'+hourTxt[h]+'</span>';
+					hourEm += '<span class="week-hour">'+hourTxt[h]+'</span>';
 					for(s in weekTxt){
 						hourEm += '<span id="week-'+ s +'-hour-'+ h +'"></span>';
 					}
@@ -34,23 +33,12 @@ $(function(){
 			var w = 0,
 				weekEm = '<ul>';
 			for (; w < weekTxt.length; w++) {
-				weekEm += '<li class="data-day-'+(parseInt(w) + 1)+'" data-day='+(parseInt(w) + 1)+'>'+weekTxt[w]+'</li>';
+				weekEm += '<li class="data-day-'+(parseInt(w) + 1)+'" data-day='+(parseInt(w) + 1)+'><span>'+weekTxt[w]+'</span><span class="data-date-'+(parseInt(w) + 1)+'"></span></li>';
 			};
 			weekEm += '</ul>';
 			$('.weekDate').html(weekEm);
 		}
 
-		//构建日期
-		var dateDraw = function(){
-			var i = 0,
-				dateEm = '<ul>';
-			for(;i<7;i++){
-				dateEm += '<li class="data-week-'+(parseInt(i) + 1)+'"></li>';
-			};
-			dateEm += '</ul>';
-			$('.motDate').html(dateEm);
-		}
-		
 		//填充日期
 		var fillDate = function(){
 			var curDay = initDate.getDay();
@@ -58,52 +46,89 @@ $(function(){
 				curDay = 7;
 			};
 			initDate.setDate(initDate.getDate() - curDay + 1);
-			
+
 			var myDate = new Date(initDate),
 				day = 0,
 				dateNow = new Date;
 			for(; day < 7; day++){
-				var $thisDay = $('.data-week-'+(parseInt(day) + 1)),
+				var $thisDay = $('.data-date-'+(parseInt(day) + 1)),
 					$thisWeek = $('.data-day-'+(parseInt(day) + 1)),
 					y = myDate.getFullYear(),
 					m = myDate.getMonth()+1,
 					d = myDate.getDate();
-				$thisDay.html(((m<10?'0'+m:m)+'月'+(d<10?'0'+d:d)+'日'));
+				$thisDay.html('('+(m<10?'0'+m:m)+'月'+(d<10?'0'+d:d)+'日)');
 				$thisDay.attr({
 					'data-year': y,
 					'data-month': m,
 					'data-date': d
 				});
 				if(myDate.getFullYear()==dateNow.getFullYear()&&myDate.getMonth()==dateNow.getMonth()&&myDate.getDate()==dateNow.getDate()){
-					$thisDay.html('今天').addClass('active');
-					$thisWeek.addClass('active');
+						$thisDay.html('(今天)').addClass('active');
 				};
 				myDate.setDate(myDate.getDate() + 1);
 			};
 		}
-		
+
 		//加载日期
 		var reload = function(){
-			dateDraw();
 			weekDraw();
 			hourDraw();
 			fillDate();
 		}
 		reload();
-		
+
 		//下一周
 		$('.next-week').live('click',function(){
 			initDate.setDate(initDate.getDate()+7);
 			reload();
+			overdueTime();
 		})
-		
+
 		//上一周
 		$('.prev-week').live('click',function(){
 			 var now = new Date();
-             if (initDate.getTime() > now.getTime()) {
-             	initDate.setDate(initDate.getDate() - 7);
-            	reload();
-             }
-        	
+           if (initDate.getTime() > now.getTime()) {
+					 }
+           		initDate.setDate(initDate.getDate() - 7);
+          		reload();
+					 overdueTime();
+		})
+
+
+		//过期时间
+		initDate.setDate(initDate.getDate() + 7);
+		var overdueTime = function(){
+			for (var w = 0; w < 7; w++){
+				var nowDate = new Date();
+				// var nowYear = nowDate.getFullYear();
+				// var nowMon = nowDate.getMonth() + 1;
+				// var nowDay = nowDate.getDate();
+				// var nowWeek = nowDate.getDay();
+				// var nowHours = nowDate.getHours();
+				// var conDate = $('span[class^="data-date-'+w+'"]');
+				// var conYear = conDate.attr('data-year');
+				// var conMon = conDate.attr('data-month');
+				// var conDay = conDate.attr('data-date');
+				// var tDay = conYear+'/'+conMon+'/'+conDay;
+				// var nDay = nowYear+'/'+nowMon+'/'+nowDay;
+				// && hourTxt[x].substring(0,2) <= nowHours && w <= nowWeek && tDay <= nDay
+				for (var x = 0;x < 14;x++){
+						if(nowDate.getTime()>initDate.getTime()){
+							$('span[id="week-'+w+'-hour-'+x+'"]').addClass('disabled');
+						}
+				}
+			}
+		}
+		overdueTime();
+
+
+		//选中
+		$('span[id^=week][class!="disabled"]').live('click',function(){
+				var _self = $(this);
+				var time = _self.siblings('.week-hour').html();
+
+				console.log(time);
+				_self.addClass('selected');
+
 		})
 })
