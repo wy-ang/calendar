3e0b7e6d6f29573b7e89fe11ab1d7ad0ef5785de
wangyang <wyang1117@msn.com>
2016-07-15 15:05:43 +0800

    修改月份展示

--- month.js
+++ month.js
@@ -21,22 +21,24 @@ $(function(){
 		weekDraw();
 
 		var dateDraw = function(){
-				var day = initDate.getDay();
-				if(day == 0){
-						day = 7;
-				}
-				initDate.setDate(initDate.getDate() - day + 1);
+				var getNewDate = new Date(),
+						getYear = getNewDate.getFullYear(),
+						getMon = getNewDate.getMonth(),
+						getDay = getNewDate.getDate();
+				var setNewDate = new Date(getYear,getMon,1),
+						firstDay = setNewDate.getDay();
 
 
 				var dateWrap = [];
-				var nowDate = new Date(initDate);
+				var nowDate = new Date(setNewDate);
 				var weekTxt = ['周一', '周二', '周三', '周四', '周五', '周六', '周日'];
 
 				for(var i = 0;i<6;i++){
 					dateWrap.push('<ul>');
-						for (j in weekTxt) {
-								dateWrap.push('<li date='+nowDate.toLocaleString()+'>'+nowDate.getDate()+'</li>');
-								nowDate.setDate(nowDate.getDate() + 1);
+						for (var j =0;j<7;j++) {
+								
+								dateWrap.push('<li date='+nowDate.toLocaleString()+' week='+nowDate.getDay()+'>'+nowDate.getDate()+'</li>');
+								 nowDate.setDate(nowDate.getDate() + 1);
 						}
 						dateWrap.push('</ul>');
 				}
