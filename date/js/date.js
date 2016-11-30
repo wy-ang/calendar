05cb4e2e828a72f9307f0d82e642afe6235a61ad
wyang <wyang1117@msn.com>
2016-12-01 00:29:29 +0800

    完成日历手机端自适应

--- date/js/date.js
+++ date/js/date.js
@@ -1,5 +1,5 @@
 /**
- * 日历插件 
+ * 日历插件
  * 支持正常日期展示及场次预约
  * 汪洋
  **/
@@ -22,7 +22,7 @@ $(function(){
 				initDate.setDate(initDate.getDate() - curDay);
 				var tDay = new Date(initDate);
 
-				for(var i = 0;i<curMonthDays+12;i++){
+				for(var i = 0;i<curMonthDays+11;i++){
 					if(i%7==0){
 						hourEm += '<tr>';
 					}
@@ -121,29 +121,54 @@ $(function(){
 			date = _self.attr('data');
 
 			$.ajax({
-				url:'season.json',
+				url:'https://raw.githubusercontent.com/wy-ang/once/master/season.json',
 				dataType:'json',
-				type:'POST',
+				type:'get',
 				data:{
 					aaa:111
 				},
 				success:function(data){
+					var $layer = '<div class="container layer visible-xs-block">';
+							$layer += '<div class="row">';
+							$layer += '<span class="arrow"></span>';
 					for(var i in data){
+						var date = data[i].date,
+						size = data[i].size;
 						for(var j in data[i].list){
-							$('td[data='+data[i].date+']').append(data[i].list[j].type==0?'<a class="label label-primary notSed visible-lg-block" type="0"><i class="glyphicon"></i> '+data[i].list[j].fieldName+'</a>':'<a class="label label-danger visible-lg-block"><i class="glyphicon glyphicon-remove" type="1"></i> ' +data[i].list[j].fieldName+'</a>');
-							$('td[data='+data[i].date+']').append('<span class="tag visible-xs-block" type="0">'+data[i].size+'</span>');
-							console.log(data[i].list[j].fieldName);
-							$("td").tooltip({
-								trigger:'hover',
-								html:true,
-								title:'<div class=" visible-xs-block"><a class="label label-primary notSed" type="0"><i class="glyphicon"></i> '+data[i].list[j].fieldName+'</a></div>',
-								placement:'bottom'
-							});
+							!(function(i){
+								var fieldName = data[i].list[j].fieldName,
+								type = data[i].list[j].type;
+
+								$('td[data='+date+']').append(type==0?'<a class="label label-primary notSed visible-lg-block" type="0"><i class="glyphicon"></i> '+fieldName+'</a>':'<a class="label label-danger visible-lg-block"><i class="glyphicon glyphicon-remove" type="1"></i> ' +fieldName+'</a>');
+								$layer += '<span class="col-xs-10 col-sm-10 label label-primary">'+fieldName+'</span>';
+						})(i);
+
+							// $("td").tooltip({
+							// 	trigger:'hover',
+							// 	html:true,
+							// 	title:'<div class=" visible-xs-block"><a class="label label-primary notSed" type="0"><i class="glyphicon"></i> '+data[i].list[j].fieldName+'</a></div>',
+							// 	placement:'bottom'
+							// });
 						}
-						/*console.log(data[i].date);
-						console.log(data[i].season);
-						console.log(data[i].type);*/
+						$('td[data='+date+']').append('<span class="tag visible-xs-block" type="0">'+size+'</span>');
 					}
+					$layer += '</div>';
+					$layer += '</div>';
+					$('tbody td').live('click',function(){
+						var _self = $(this);
+						var x = _self.offset();
+						var w = _self.outerWidth();
+						var h = _self.outerHeight();
+						_self.append($layer);
+					//	_self.css('background','#428bca').siblings().css('background','none');
+						$('.layer').css({
+							top:x.top+h+6,
+							left:0
+						});
+						$('.arrow').css({
+							left:x.left+w/2
+						})
+					});
 				}
 			})
 		}
