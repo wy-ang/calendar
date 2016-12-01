050198ce9433d2cdd0df6fed70fbd6d287ca1fac
wyang <wyang1117@msn.com>
2016-12-01 23:22:22 +0800

    响应式设计

--- date/js/date.js
+++ date/js/date.js
@@ -128,19 +128,32 @@ $(function(){
 					aaa:111
 				},
 				success:function(data){
-					var $layer = '<div class="container layer visible-xs-block">';
-							$layer += '<div class="row">';
-							$layer += '<span class="arrow"></span>';
 					for(var i in data){
 						var date = data[i].date,
 						size = data[i].size;
+						$('td[data='+date+']').append('');
 						for(var j in data[i].list){
 							!(function(i){
 								var fieldName = data[i].list[j].fieldName,
 								type = data[i].list[j].type;
 
 								$('td[data='+date+']').append(type==0?'<a class="label label-primary notSed visible-lg-block" type="0"><i class="glyphicon"></i> '+fieldName+'</a>':'<a class="label label-danger visible-lg-block"><i class="glyphicon glyphicon-remove" type="1"></i> ' +fieldName+'</a>');
-								$layer += '<span class="col-xs-10 col-sm-10 label label-primary">'+fieldName+'</span>';
+								//$('td[data='+date+']').find('.row').append('<span class="col-xs-10 col-sm-10 label label-primary">'+fieldName+'</span>');
+								$('td[data='+date+']').live('click',function(){
+									var _self = $(this);
+									var x = _self.offset();
+									var w = _self.outerWidth();
+									var h = _self.outerHeight();
+									$('.layer .row').append('<span class="col-xs-10 col-sm-10 label label-primary">'+fieldName+'</span>');
+									//	_self.css('background','#428bca').siblings().css('background','none');
+									$('.layer').css({
+										top:x.top+h+6,
+										left:0
+									});
+									$('.arrow').css({
+										left:x.left+w/2
+									})
+								});
 						})(i);
 
 							// $("td").tooltip({
@@ -152,23 +165,6 @@ $(function(){
 						}
 						$('td[data='+date+']').append('<span class="tag visible-xs-block" type="0">'+size+'</span>');
 					}
-					$layer += '</div>';
-					$layer += '</div>';
-					$('tbody td').live('click',function(){
-						var _self = $(this);
-						var x = _self.offset();
-						var w = _self.outerWidth();
-						var h = _self.outerHeight();
-						_self.append($layer);
-					//	_self.css('background','#428bca').siblings().css('background','none');
-						$('.layer').css({
-							top:x.top+h+6,
-							left:0
-						});
-						$('.arrow').css({
-							left:x.left+w/2
-						})
-					});
 				}
 			})
 		}
